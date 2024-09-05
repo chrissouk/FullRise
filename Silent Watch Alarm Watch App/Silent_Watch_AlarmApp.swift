48746fd71e82b9a756db6ed83ce16dad3e70f73f
Soukerman <142164991+Soukerman@users.noreply.github.com>
2024-09-05 13:39:52 -0400

    Initial Commit

---
+++ Silent Watch Alarm Watch App/Silent_Watch_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silent_Watch_AlarmApp.swift
+//  Silent Watch Alarm Watch App
+//
+//  Created by Chris Souk on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Silent_Watch_Alarm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Silent Watch Alarm/Silent_Watch_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silent_Watch_AlarmApp.swift
+//  Silent Watch Alarm
+//
+//  Created by Chris Souk on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Silent_Watch_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
